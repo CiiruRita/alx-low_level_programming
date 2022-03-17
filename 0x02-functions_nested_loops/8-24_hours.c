a4b711d37072965434225fc00284cb42a9d18868
CiiruRita <rciiru@gmail.com>
2022-03-17 08:59:54 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square -  print squares
+ * @size : defines number ofi #
+ * Return: On success int
+ * On error, 0 is returned, and error is set appropriately.
+ */
+void print_square(int size)
+{
+int i = 0;
+int j = 0;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
